ecb00b90dc5d93e501f55b92b4cffb603c1bb7ee
David Cheng <chengshair@gmail.com>
2020-08-14 16:27:48 +0800

    implement screenshot image drawing

--- view_javascript/issueEditImage.js
+++ view_javascript/issueEditImage.js
@@ -1,15 +1,112 @@
 
-
+var context;
 window.addEventListener("load", function(){
     var img = document.querySelector('#editingImage');
-    img.width = 1280;
+    img.width = 1150;
     img.setAttribute("object-fit", "cover");
     img.src = window.dataURI;
-    //console.log(window.dataURI);
+
+    //load convas after image is loaded
+    if (img.complete) {
+        setUpDraw();
+    } else {
+        img.addEventListener('load', setUpDraw);
+    }
 });
 
+
+
 document.querySelector('#doneEditImage').addEventListener('click', function(e) {
-    html2canvas(document.querySelector("#convasCrop"), {scale:1}).then(canvas => {
+
+    var canvas = document.getElementById('canvas');
+    var canvasURL = canvas.toDataURL();
+    var canvasImage = document.getElementById('canvasImage');
+    canvasImage.src = canvasURL;
+    if (canvasImage.complete) {
+        startCrop();
+    } else {
+        canvasImage.addEventListener('load', startCrop);
+    }
+});
+
+
+var convasCropOffsetTop = document.getElementById('canvasCrop').offsetTop;
+var convasCropOffsetLeft = document.getElementById('canvasCrop').offsetLeft;
+
+$('#canvas').mousedown(function(e){
+
+    
+    var mouseX = e.pageX - this.offsetLeft - convasCropOffsetLeft;
+    var mouseY = e.pageY - this.offsetTop - convasCropOffsetTop;
+    paint = true;
+    addClick(mouseX, mouseY);
+    redraw();
+});
+
+$('#canvas').mousemove(function(e){
+    if(paint){
+      addClick(e.pageX - this.offsetLeft - convasCropOffsetLeft, e.pageY - this.offsetTop - convasCropOffsetTop, true);
+      redraw();
+    }
+});
+
+$('#canvas').mouseup(function(e){
+paint = false;
+});
+
+$('#canvas').mouseleave(function(e){
+    paint = false;
+  });
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+}
+
+function redraw(){
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+    
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+              
+    for(var i=0; i < clickX.length; i++) {		
+      context.beginPath();
+      if(clickDrag[i] && i){
+        context.moveTo(clickX[i-1], clickY[i-1]);
+       }else{
+         context.moveTo(clickX[i]-1, clickY[i]);
+       }
+       context.lineTo(clickX[i], clickY[i]);
+       context.closePath();
+       context.stroke();
+    }
+}
+
+
+function getPreviewImageItem(url){
+
+    var img = document.createElement('img');
+    img.width = 300;
+    img.setAttribute("object-fit", "cover");
+    img.src = url;
+
+    var item = document.createElement('div');
+    item.classList.add("item");
+    item.appendChild(img);
+
+    return item
+}
+
+function startCrop(){
+    html2canvas(document.querySelector("#canvasCrop"), {scale:1}).then(canvas => {
         var imageExist = document.getElementById('editedImage');
         if(imageExist != undefined)
             return;
@@ -23,19 +120,21 @@ document.querySelector('#doneEditImage').addEventListener('click', function(e) {
         window.opener.editingImages.push(url);
         window.close();
     });
-    
+}
 
-});
+function setUpDraw(){
 
-function getPreviewImageItem(url){
-    var img = document.createElement('img');
-    img.width = 300;
-    img.setAttribute("object-fit", "cover");
-    img.src = url;
 
-    var item = document.createElement('div');
-    item.classList.add("item");
-    item.appendChild(img);
 
-    return item
+    var img = document.querySelector('#editingImage');
+    var canvas = document.querySelector("#canvas");
+    canvas.width = img.width-1;
+    canvas.height = img.height-1;
+
+    var canvasCrop = document.querySelector('#canvasCrop');
+    canvasCrop.style.width = img.width-1;
+    canvasCrop.style.height = img.height-1;
+
+    context = document.getElementById('canvas').getContext("2d");
+
 }
