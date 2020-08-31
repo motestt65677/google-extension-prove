826c09acbef4477f75756452186b2e8edb547b74
David Cheng <chengshair@gmail.com>
2020-08-31 16:26:12 +0800

    modify upload issue to gitlab. add gitlab icon when sync. change sync to async = false

--- view_javascript/issueList.js
+++ view_javascript/issueList.js
@@ -28,82 +28,129 @@ $('[data-tab]').click(function(){
     loadIssues($(this).data('tab'))
 });
 $('#gitlab').click(function(){
+    $('#gitlab').prop('disabled', true);
     // var id = "_1598507751746";
-    var id = "_1597817455434";
-    chrome.storage.local.get([id], function (item) {
-        var url = "http://localhost:9990/addFileToGitLabProject";
-        var privateToken = "jirUTEUKh9zj-U5HTwg7"
-        var projectId = "20611880";
-        var thisIssue = item[id];
-        var images = thisIssue["images"];
-        var imageLinks = [];
-        for(var i = 0; i < images.length; i++){
-            var imageBase64 = item[id]["images"][i].replace("data:image/png;base64,","");
-            var fileName = guidGenerator() + '.png';
+    // var id = "_1597817455434";
+    var url = "http://localhost:9990/addFileToGitLabProject";
+    var privateToken = "jirUTEUKh9zj-U5HTwg7"
+    var projectId = "20611880";
+
+    // var waitingAjax = [];
+    chrome.storage.local.get(null, function (items) {
+        // the input argument is ALWAYS an object containing the queried keys
+        // so we select the key we need
+        var allKeys = Object.keys(items);
+        for(var i = 0; i< allKeys.length; i++){
+            var thisIssue = items[allKeys[i]];
+            var thisIssueId = allKeys[i];
+            var thisStatus = thisIssue.status;
+            var imageLinks = [];
+
+            if(thisStatus != "open")
+                continue;
+
+            if("gitlab" in thisIssue)
+                continue;
+
+            // var thisIssue = item[id];
+            var images = thisIssue["images"];
+            for(var j = 0; j < images.length; j++){
+                var imageBase64 = thisIssue["images"][j].replace("data:image/png;base64,","");
+                var fileName = guidGenerator() + '.png';
+        
+                var blob = base64ToBlob(imageBase64, 'image/png');                
+                var formData = new FormData();
+                formData.append('image', blob);
+                formData.append('privateToken', privateToken);
+                formData.append('projectId', projectId);
+                formData.append('fileName', fileName);
+                formData.append('imageOrder', j);
+                
+                $.ajax({
+                    url: url, 
+                    type: "POST", 
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    data: formData,
+                    async: false,
+                    success: function(result) {
+                        // var number = result["number"];
+                        // var markdown = result["markdown"];
+                        imageLinks.push(result["markdown"]);
     
-            var blob = base64ToBlob(imageBase64, 'image/png');                
-            var formData = new FormData();
-            formData.append('image', blob);
-            formData.append('privateToken', privateToken);
-            formData.append('projectId', projectId);
-            formData.append('fileName', fileName);
-            formData.append('imageOrder', i);
-            
+                        // console.log(result);
+                        // var data = JSON.parse(result);
+
+                        // Run the code here that needs
+                        //    to access the data returned
+                        // return data;
+                    },
+                
+                }).done(function(e){
+                    // alert('done!');
+                });
+
+            }
+
+            var title = thisIssue["title"];
+            // description = "a\r\n\r\nb \r\n c \r\n\r d";
+            var description = "Priority: " + thisIssue["priority"] + "\r\n\r\n"
+            + "Browser: " + thisIssue["browser"] + "\r\n\r\n"
+            + "Url: " + thisIssue["url"] + "\r\n\r\n"
+            + "Expected Result: " + thisIssue["expected_result"] + "\r\n\r\n"
+            + "Actual Result: " + thisIssue["actual_result"] + "\r\n\r\n"
+            + "Steps to Reproduce: " + thisIssue["steps"] + "\r\n\r\n"
+            + "Description: " + thisIssue["description"] + "\r\n\r\n"
+            ;
+
+            for(var k = 0; k < imageLinks.length; k++){
+                description += imageLinks[k] + "\r\n\r\n";
+            }
+
             $.ajax({
-                url: url, 
-                type: "POST", 
-                cache: false,
-                contentType: false,
-                processData: false,
-                data: formData,
-                success: function(result) {
-                    // console.log(result);
-                    // var data = JSON.parse(result);
-                    var number = result["number"];
-                    var markdown = result["markdown"];
-                    imageLinks[number] = markdown;
-                    if(imageLinks.length == images.length &&  !imageLinks.includes(undefined)){
-                        //upload issue
-                        title = thisIssue["title"];
-                        // description = "a\r\n\r\nb \r\n c \r\n\r d";
-                        description = "Priority: " + thisIssue["priority"] + "\r\n\r\n"
-                        + "Browser: " + thisIssue["browser"] + "\r\n\r\n"
-                        + "Url: " + thisIssue["url"] + "\r\n\r\n"
-                        + "Expected Result: " + thisIssue["expected_result"] + "\r\n\r\n"
-                        + "Actual Result: " + thisIssue["actual_result"] + "\r\n\r\n"
-                        + "Steps to Reproduce: " + thisIssue["steps"] + "\r\n\r\n"
-                        + "Description: " + thisIssue["description"] + "\r\n\r\n"
-                        ;
+                type: "POST",
+                async: false,
+                url: "https://gitlab.com/api/v4/projects/"+projectId+"/issues",
+                // The key needs to match your method's input parameter (case-sensitive).
+                headers: {"PRIVATE-TOKEN": "jirUTEUKh9zj-U5HTwg7"},
+                data: {title: title, description: description},
+                // contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                
+                success: function(data){
+                    console.log("done");
 
-                        for(var j = 0; j < imageLinks.length; j++){
-                            description += imageLinks[j] + "\r\n\r\n";
-                        }
+                    console.log(data);
 
-                        $.ajax({
-                            type: "POST",
-                            url: "https://gitlab.com/api/v4/projects/"+projectId+"/issues",
-                            // The key needs to match your method's input parameter (case-sensitive).
-                            headers: {"PRIVATE-TOKEN": "jirUTEUKh9zj-U5HTwg7"},
-                            data: {title: title, description: description},
-                            // contentType: "application/json; charset=utf-8",
-                            dataType: "json",
-                            success: function(data){alert(data);},
-                            failure: function(errMsg) {
-                                alert(errMsg);
-                            }
-                        });
+                    //add gitlab info
+                    // var thisItem = item[thisIssueId];
+                    thisIssue.gitlab = data;
+                    // console.log(thisItem);
+                    chrome.storage.local.set({[thisIssueId]: thisIssue}); 
+                    
+                    
+                    // waitingAjax.push(thisIssueId);
+                    // var index = waitingAjax.indexOf(thisIssueId);
+                    // if (index > -1) 
+                    //     waitingAjax.splice(index, 1);
+                    
+                    // if(waitingAjax.length == 0)
+                    //     $('#gitlab').prop('disabled', false);
 
-                    }
-                    // Run the code here that needs
-                    //    to access the data returned
-                    // return data;
                 },
-         
-            }).done(function(e){
-                // alert('done!');
+                failure: function(errMsg) {
+                    alert(errMsg);
+                }
             });
         }
+
+        $('#gitlab').prop('disabled', false);
+
     });
+
+    // $('#gitlab').prop('disabled', false);
+
 });
 
 function base64ToBlob(base64, mime) 
@@ -141,15 +188,30 @@ function guidGenerator() {
 function toInfoPage(){
     window.location.href = "/view/issueInfo.html?id=" + this.id;
 }
-
+function removeGitlabTags(){
+    chrome.storage.local.get(null, function (items) {
+        var allKeys = Object.keys(items);
+        for(var i = 0; i< allKeys.length; i++){
+            var thisIssue = items[allKeys[i]];
+            var thisIssueId = thisIssue.id;
+            // console.log(thisIssue);
+            delete thisIssue["gitlab"]; 
+            chrome.storage.local.set({[thisIssueId]: thisIssue}); 
+        }
+    });
+}
 function loadIssues(status){
+
+    // removeGitlabTags();
+
+
+
     // status types: open, closed, all
     document.getElementById('issues').innerHTML = "";
     chrome.storage.local.get(null, function (items) {
         // the input argument is ALWAYS an object containing the queried keys
         // so we select the key we need
         var allKeys = Object.keys(items);
-
         for(var i = 0; i< allKeys.length; i++){
             var thisIssue = items[allKeys[i]];
             var thisStatus = thisIssue.status;
@@ -161,13 +223,31 @@ function loadIssues(status){
             item.id = thisIssue.id;
             item.onclick=toInfoPage;
             var content = document.createElement('div');
-            var header = document.createElement('div');
+            var header = document.createElement('span');
             item.classList.add('item');
             content.classList.add('content');
             header.classList.add('header');
             header.innerHTML = thisIssue.title;
+
+            //adding gitlab icon if issue is on gitlab
+            if("gitlab" in thisIssue){
+                // console.log(thisIssue);
+                var gitlabSpan = document.createElement('span');
+                gitlabSpan.classList.add("gitlabSpan");
+                var gitlabIcon = document.createElement('i');
+                gitlabIcon.classList.add("gitlab");
+                gitlabIcon.classList.add("icon");
+                var issueId = document.createElement('span');
+                issueId.innerHTML = "#" + thisIssue.gitlab.iid;
+                gitlabSpan.appendChild(gitlabIcon);
+                gitlabSpan.appendChild(issueId);
+
+                header.appendChild(gitlabSpan);
+            }
             content.appendChild(header);
-            item.appendChild(content)
+
+            item.appendChild(content);
+
             document.getElementById("issues").appendChild(item);
         }
     });
